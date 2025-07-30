84e43541f96f5efbc5c554c0dd64ab2ac589c47c
laíza <164918287+laizagranville@users.noreply.github.com>
2025-07-30 13:36:32 -0300

    .kl

--- scriop.js
+++ scriop.js
@@ -1,5 +1,6 @@
 function inserirNome(){
     let nomeUsuario = prompt("Qual seu nome");
       let elemento = document.querySelector("#nome-usuario");
-      console.textContent = nomeUsuario;
+      console.log(elemento)
+      elemento.textContent = nomeUsuario;
 }
