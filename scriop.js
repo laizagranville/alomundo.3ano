0507e113164fd23a986aae39f6d83376931d6553
laíza <164918287+laizagranville@users.noreply.github.com>
2025-08-01 15:10:36 -0300

    .

--- scriop.js
+++ scriop.js
@@ -9,3 +9,11 @@ const lista = document.querySelector('#lista');
 lista.textContent = linguagens[0];
 lista.textContent = linguagens[1];
 lista.textContent = linguagens[2];
+
+
+let aluno1 ={
+  nome:'Laíza',
+  idade: 18,
+  anoletivo:'Esino Médio',
+  materiasFavoritas:['Educação Física e Programação']
+}
