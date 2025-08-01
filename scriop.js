25424ffc45e7c58e1deb40c7533ebc5da33e2a07
laíza <164918287+laizagranville@users.noreply.github.com>
2025-08-01 15:00:46 -0300

    .

--- scriop.js
+++ scriop.js
@@ -5,3 +5,7 @@ function inserirNome(){
       elemento.textContent = nomeUsuario;
 }
 const lista = document.querySelector('#lista');
+
+lista.textContent = linguagem[0];
+lista.textContent = linguagem[1];
+lista.textContent = linguagem[2];
