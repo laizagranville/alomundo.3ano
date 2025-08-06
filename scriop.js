741146207f695ffc84907dfa8aeca18aab1bb7b3
laíza <164918287+laizagranville@users.noreply.github.com>
2025-08-06 13:15:42 -0300

    .

--- scriop.js
+++ scriop.js
@@ -22,3 +22,11 @@ console.log(aluno1.nome);
 console.log(aluno1.idade);
 console.log(aluno1.materiasFavoritas);
 console.log(aluno1.materiasFavoritas[1]);
+
+const objeto = document.querySelector('#aluno');
+objeto.textContent = aluno1.nome;
+objeto.textContent = aluno1.idade;
+objeto.textContent = aluno1.materiasFavoritas[1];
+
+
+
